3e2ebadf0888d5780a5657cdde7c11afd50b776f
Mitchell <MitchellTurnbaughCProg@gmail.com>
2026-05-05 10:38:48 -0400

    Day 2

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+function scratch(square){
+    square.innerHTML = square.id
+    
+}
+function randint(max ,startAt = 0){
+    return Math.floor(Math.random() * max) + startAt
+}
+function choice(data){
+    return data[randint(data.length)]
+}
 const squares = document.getElementsByClassName("square");
 for(square of squares){
-    square.textContent = "Winner"
+    words = ["Winner","Loser"];
+    square.id = `<h6>${choice(words)}</h6>$${randint(2,1)}`;
 }
