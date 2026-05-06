8547e5ca718cd4d5e4f340f8a10d61576a7d3190
Mitchell <MitchellTurnbaughCProg@gmail.com>
2026-05-06 10:40:31 -0400

    day 3

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function scratch(square){
+    if(square.innerHTML === ""){
     square.innerHTML = square.id
-    
+        if(square.firstChild.textContent === "Winner"){
+            const winnings = document.getElementById("winnings");
+            const total = document.getElementById("total");
+            winnings.textContent = `Amount Won This Round: $${Number(winnings.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`;
+            total.textContent = `Total Amount Won: $${Number(total.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`
+        }
+    }
 }
-function randint(max ,startAt = 0){
-    return Math.floor(Math.random() * max) + startAt
+function getNewCard(){
+    const squares = document.getElementsByClassName("square");
+    for(square of squares){
+        square.innerHTML = ""
+        words = ["Winner","Loser"];
+        square.id = `<h6>${choice(words)}</h6><p>$${randint(2,1)}</p>`;
+    }
+}
+function randint(max,startAt = 0){
+    return Math.floor(Math.random() * max) + startAt;
 }
 function choice(data){
-    return data[randint(data.length)]
+    return data[randint(data.length)];
 }
 const squares = document.getElementsByClassName("square");
 for(square of squares){
     words = ["Winner","Loser"];
-    square.id = `<h6>${choice(words)}</h6>$${randint(2,1)}`;
+    square.id = `<h6>${choice(words)}</h6><p>$${randint(2,1)}</p>`;
 }
