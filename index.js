6f323e398c99ac48e7096ebcfbb4d2927f8dbcce
Mitchell <MitchellTurnbaughCProg@gmail.com>
2026-05-11 10:41:05 -0400

    Day 6

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 let squaresRevealed = 0;
 const words = [
-    "Winner","Not a winner","Loser", "Try again","Maybe next time",
+    "Winner", "Not a winner", "Loser", "Try again", "Maybe next time", "Next time's the charm",
     "<img id = 'tryAgainImg' src = 'resources/index/try_again.jpg'>"
 ];
 function sleep(time){
     return new Promise((resolve) => setTimeout(resolve,time));
 }
 async function scratch(square){
+    const message = document.getElementById("message");
     if(square.innerHTML === ""){
         squaresRevealed += 1;
+        message.textContent = `Your ticket still has ${16 - squaresRevealed} unscratched squares`
         square.innerHTML = square.id;
             if(square.firstChild.textContent === "Winner"){
                 const winnings = document.getElementById("winnings");
                 const total = document.getElementById("total");
-                const netResults = document.getElementById("netResults");
+                const netResult = document.getElementById("netResult");
                 winnings.textContent = `$${Number(winnings.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`;
                 total.textContent = `$${Number(total.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`;
-                if(netResults.textContent.startsWith("-")){
-                    let current = netResults.textContent.split("$")[1] - square.children[1].textContent.replace("$","");
+                if(netResult.textContent.startsWith("-")){
+                    let current = netResult.textContent.split("$")[1] - square.children[1].textContent.replace("$","");
                     if(current > 0){
-                        netResults.textContent = `-$${current}`;
+                        netResult.textContent = `-$${current}`;
                     }else{
-                        netResults.textContent = `$${Math.abs(current)}`;
-                        netResults.style.color = "#1a652a";
+                        netResult.textContent = `$${Math.abs(current)}`;
+                        netResult.style.color = "#1a652a";
                     }
                 }else{
-                    netResults.textContent = `$${Number(netResults.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`;
+                    netResult.textContent = `$${Number(netResult.textContent.split("$")[1]) + Number(square.children[1].textContent.replace("$",""))}`;
                 }
             }
         }
-        if(squaresRevealed === 16){
-            const message = document.getElementById("message");
-            message.textContent = `You won $${winnings.textContent.split("$")[1]}.`;
-            await sleep(2000);
-            getNewCard();
+    if(squaresRevealed === 16){
+        if(Number(winnings.textContent.split("$")[1]) > 0){
+            message.innerHTML = `You won <span class = "green">${winnings.textContent}</span> this round for a total of <span class = "green">${total.textContent}</span> won overall with a net result of `;
+            span = document.createElement("span");
+            if(netResult.textContent.replace("$","") - 2 >= 0){
+                span.style.color = "rgb(26, 101, 42)";
+                span.textContent = `$${netResult.textContent.replace("$","") - 2}`;
+                message.appendChild(span);
+            }else{
+                message.innerHTML += `<span style = "color: red;">-$${Math.abs(netResult.textContent.replace("$","") - 2)}</span>`;
+            }
+        }else{
+            message.innerHTML = `You didn't win anything, you have a total of <span class = "green">${total.textContent}</span>, you have a net result of `;
+            if(netResult.textContent.replace("$","") - 2 >= 0){
+                message.innerHTML += `<span style = "color: rgb(26, 101, 42);"">$${netResult.textContent.replace("$","") - 2}</span> try again`;
+            }else{
+                message.innerHTML += `<span style = "color: red;">-$${Math.abs(netResult.textContent.replace("$","") - 2)}</span>`
+            }
         }
+        await sleep(2000);
+        getNewCard();
+    }
+}
+function revealQrCode(div){
+    div.innerHTML = "<img src = 'resources/index/qr_code.png' alt = 'QR code'>";
 }
 function getNewCard(){
     squaresRevealed = 0;
@@ -43,7 +64,7 @@ function getNewCard(){
     winnings.textContent = "$0";
     const cardNumber = document.getElementById("cardNumber");
     cardNumber.textContent = String(Number(cardNumber.textContent) + 1).padStart(3,0)
-    const netResult = document.getElementById("netResults");
+    const netResult = document.getElementById("netResult");
     if(netResult.textContent.replace("$","") <= 0){
         netResult.style.color = "red";
         netResult.textContent = `-$${Number(netResult.textContent.split("$")[1]) + 2}`;
@@ -58,9 +79,9 @@ function getNewCard(){
         }
     }
     const message = document.getElementById("message");
-    message.textContent = "Your ticket still has unscratched squares.";
-    if(netResults.style.color === "red"){
-        message.textContent += " Your still in the red";
+    message.textContent = "Your ticket still has 16 unscratched squares.";
+    if(netResult.style.color === "red"){
+        message.textContent += " You're still in the red";
     }
     for(square of squares){
         square.innerHTML = "";
